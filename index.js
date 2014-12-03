72b470ecab9c11a3450ecebf5b4e674a7d3c425e
Guido Gallopyn”git config --global user.name Guido <guidogallopyn@gmail.com>
2014-12-03 06:05:24 +0000

    small test with html file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
+  var buffer = fs.readFileSync("./index.html");
+  var page = buffer.toString('utf-8');
+  response.send(page);
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
